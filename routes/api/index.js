731b593ebf82189ff5e01dedb1e49bbf8139e922
Chris San <christianes322@gmail.com>
2023-11-24 19:09:04 -0600

    created index js for routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require(`express`).Router();
 
 const userRoutes = require(`./user-routes`);
-const thoughtRoutes = require(`./user-routes`);
+
+const thoughtsRoutes = require(`./thought-routes`);
 
 router.use('/users', userRoutes);
 
